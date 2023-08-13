fa0212f7f06cd80bb93dc3f1ec17741b6a76469f
Steffen Plunder <steffen.plunder@web.de>
2023-08-13 20:13:29 +0900

    more complicated demo script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** This is a setup funciton. */
 function setup() {
-  var canvas = createCanvas(500, 400);
+  var canvas = createCanvas(400, 500);
   canvas.parent("sketch-holder");
 
   init();
@@ -9,6 +9,11 @@ function setup() {
 /** This is the main interface with tweakpane */
 const p = {
   dt: 0.01,
+
+  /** Some constants which are not subject to change! */
+  phys_width: 120, // physical units of the width
+  phys_height: 100, // physical units of the height
+  phys_center: { x: 60, y: 50 }, // physical center of the canvas
 };
 
 /** A second dict witch holds the current simulation state */
@@ -19,23 +24,18 @@ function init() {
   s.t = 0.0;
 }
 
-/** Finally, some constants which are not subject to change! */
-const constants = {
-  phys_width: 100, // physical units of the width
-  phys_height: 100, // physical units of the height
-};
-
 function scaleCanvas() {
-  // we fit a square of phys_width x phys_height into the canvas
-  const ratioX = width / constants.phys_width;
-  const ratioY = height / constants.phys_height;
+  // we fit a rectangle of phys_width x phys_height into the canvas
+  const ratioX = width / p.phys_width;
+  const ratioY = height / p.phys_height;
   if (ratioX < ratioY) {
-    translate(0, (height - width) / 2);
+    translate(0, (height - p.phys_height * ratioX) / 2);
     scale(ratioX, ratioX);
   } else {
-    translate((width - height) / 2, 0);
+    translate((width - p.phys_width * ratioY) / 2, 0);
     scale(ratioY, ratioY);
   }
+  translate(p.phys_center.x, p.phys_center.y);
 }
 
 /** This is a draw function. */
@@ -43,9 +43,14 @@ function draw() {
   scaleCanvas();
   background(255);
 
-  rect(2, 2, constants.phys_width - 4, constants.phys_height - 4);
+  rect(
+    -p.phys_width / 2 + 2,
+    -p.phys_height / 2 + 2,
+    p.phys_width - 4,
+    p.phys_height - 4
+  );
 
   s.t += p.dt;
 
-  circle(50 + 50 * sin(s.t), 50 + 50 * cos(s.t), 10);
+  circle(50 * sin(s.t), 50 * cos(s.t), 10);
 }
